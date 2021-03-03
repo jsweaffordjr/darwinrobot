1983d9dfd5885ea6e36ed4f5d12efaa65fe28e05
Jerry <jsweaffordjr@gmail.com>
2021-03-03 07:44:52 -0600

    added comments to GetJointAngles and MultiCPG

--- GetJointAngles.m
+++ GetJointAngles.m
@@ -1,29 +1,34 @@
 function angles = GetJointAngles(pfn,afn,params,phase,x,velocity)
-% this function computes joint angles for a given phase, cycle position
+% this function computes joint angles for a given phase (0 or 1), cycle position
 % (0 <= x <= 1), and velocity parameters
 
-    % find joint angle functions for walking in place:
-    j = fieldnames(pfn);
-    angles = zeros(length(j),1);
+%% find joint angle functions for walking in place:
+    j = fieldnames(pfn); % array of joint names
+    angles = zeros(length(j),1); % initialize joint angles at zero
     for i = 1:length(j)
-        if phase
+        % for each joint, set parameters based on phase
+        if phase % if robot is in 'phase', use 'phase' parameters for each joint
            a = pfn.(j{i}).offset;
            b = pfn.(j{i}).scale;
            c = pfn.(j{i}).in_offset;
            d = pfn.(j{i}).in_scale;
-        else
+        else % if robot is in 'antiphase', use 'antiphase' parameters for each joint
            a = afn.(j{i}).offset;
            b = afn.(j{i}).scale;
            c = afn.(j{i}).in_offset;
            d = afn.(j{i}).in_scale;
         end
-        angles(i) = a + b*sin(c + d*x);
+        % compute desired position for joint 'i', 
+        % based on parameters (a,b,c,d) and cycle position x:
+        angles(i) = a + b*sin(c + d*x); 
     end
-    % modify joint angle function for walking to move with desired velocity:
-    vx = velocity(1)*params.vx_scale;
-    vy = velocity(2)*params.vy_scale;
-    vt = velocity(3)*params.vt_scale;
+    % modify joint angle function (for walking) to move with desired velocity:
+    vx = velocity(1)*params.vx_scale; % for stepping forward/backward (forward-facing)
+    vy = velocity(2)*params.vy_scale; % for stepping sideways, left/right (forward-facing)
+    vt = velocity(3)*params.vt_scale; % for stepping so as to change body orientation
 
+    % compute terms to add to 'stepping-in-place' desired joint angles to
+    % achieve nonzero body velocity during gait
     dx = (2*x - 1)*vx;
     dy1 = x*vy;
     dy2 = (1 - x)*vy;
@@ -31,23 +36,29 @@ function angles = GetJointAngles(pfn,afn,params,phase,x,velocity)
     dt2 = (1 - x)*vt;
     
     if phase
-       angles(5) = angles(5) + dx;
-       angles(6) = angles(6) + dx;
-       angles(8) = angles(8) + dx;
-       angles(9) = angles(9) + dx;
-       
+       % add 'dx' term to sagittal joints for forward/backward gait
+       angles(5) = angles(5) + dx; % left sagittal hip
+       angles(6) = angles(6) + dx; % left sagittal ankle
+       angles(8) = angles(8) + dx; % right sagittal ankle
+       angles(9) = angles(9) + dx; % right sagittal ankle
+        
+       % for left/right (forward-facing) gait
        if vy >= 0
-          angles(2) = angles(2) - dy1;
-          angles(1) = angles(1) - dy1;
-          angles(4) = angles(4) + dy1;
-          angles(3) = angles(3) + dy1;           
+          % add/subtract 'dy1' term to/from frontal joints 
+          angles(2) = angles(2) - dy1; % left frontal hip
+          angles(1) = angles(1) - dy1; % left frontal ankle
+          angles(4) = angles(4) + dy1; % right frontal hip
+          angles(3) = angles(3) + dy1; % right frontal ankle          
        else
+          % add/subtract 'dy2' term to/from frontal joints  
           angles(2) = angles(2) + dy2;
           angles(1) = angles(1) + dy2;
           angles(4) = angles(4) - dy2;
           angles(3) = angles(3) - dy2;   
        end
        
+       % set transverse hip angles to positive/negative 'dt1' or 'dt2'
+       % based on desired turn direction
        if vt >= 0 
           angles(11) = -dt1;
           angles(12) = dt1;
@@ -56,7 +67,7 @@ function angles = GetJointAngles(pfn,afn,params,phase,x,velocity)
           angles(12) = -dt2;  
        end
        
-    else
+    else % antiphase computations:
        angles(5) = angles(5) - dx;
        angles(6) = angles(6) - dx;
        angles(8) = angles(8) - dx;
--- MultiCPG.m
+++ MultiCPG.m
@@ -2,7 +2,8 @@ function [pfn,afn,params] = MultiCPG(prevparams)
 % this function creates structures containing phase, antiphase function parameters to
 % produce multiple CPGs for various joints for a walker stepping in place
     
-    if ~isempty(prevparams)
+    if ~isempty(prevparams) % if parameters are provided,
+        % they will be used as default parameters
         swing_scale = prevparams.swing_scale;
         step_scale = prevparams.step_scale;
         step_offset = prevparams.step_offset;
@@ -10,16 +11,17 @@ function [pfn,afn,params] = MultiCPG(prevparams)
         vx_scale = prevparams.vx_scale;
         vy_scale = prevparams.vy_scale;
         vt_scale = prevparams.vt_scale;
-    else
+    else % otherwise, the following are default parameters
         swing_scale = 0;
         step_scale = 0.3;
         step_offset = 0.55;
         ankle_offset = 0;
         vx_scale = 0.5;
         vy_scale = 0.5;
-        vt_scale = 0.4;
+        vt_scale = 0.7;
     end
     
+    % output 'params' contains the parameters set above
     params.swing_scale = swing_scale;
     params.step_scale = step_scale;
     params.step_offset = step_offset;
@@ -28,6 +30,8 @@ function [pfn,afn,params] = MultiCPG(prevparams)
     params.vy_scale = vy_scale;
     params.vt_scale = vt_scale;
     
+    % generate CPGs, based on code from tutorial at:
+    % https://www.generationrobots.com/en/content/83-carry-out-simulations-and-make-your-darwin-op-walk-with-gazebo-and-ros
     f1 = SingleCPG([],-swing_scale,[],pi);
     f2 = MirrorSingleCPG(f1);
     f3 = SingleCPG(step_offset,step_scale,[],pi);
@@ -47,56 +51,64 @@ function [pfn,afn,params] = MultiCPG(prevparams)
     f7.scale = 0;
     f8 = SingleCPG(0,0,0,0);
     
-    pfn.Lankle2 = f1;
-    pfn.Lhip1 = f1;
-    pfn.Rankle2 = MirrorSingleCPG(f2);
-    pfn.Rhip1 = MirrorSingleCPG(f2);
-    pfn.Lhip2 = f3;
-    pfn.Lankle1 = f33;
-    pfn.Lknee = f4;
-    pfn.Rhip2 = MirrorSingleCPG(f5);
-    pfn.Rankle1 = MirrorSingleCPG(f6);
-    pfn.Rknee = MirrorSingleCPG(f7);
+    % sine generation structures for 'phase'
+    pfn.Lankle2 = f1; % left frontal ankle
+    pfn.Lhip1 = f1; % left frontal hip
+    pfn.Rankle2 = MirrorSingleCPG(f2); % right frontal ankle
+    pfn.Rhip1 = MirrorSingleCPG(f2); % right frontal hip
+    pfn.Lhip2 = f3; % left sagittal hip
+    pfn.Lankle1 = f33; % left sagittal ankle
+    pfn.Lknee = f4; % left knee
+    pfn.Rhip2 = MirrorSingleCPG(f5); % right sagittal hip
+    pfn.Rankle1 = MirrorSingleCPG(f6); % right sagittal ankle
+    pfn.Rknee = MirrorSingleCPG(f7); % right knee
     
-    afn.Lankle2 = f2;
-    afn.Lhip1 = f2;       
-    afn.Rankle2 = MirrorSingleCPG(f1);
-    afn.Rhip1 = MirrorSingleCPG(f1);
-    afn.Lhip2 = f5;
-    afn.Lankle1 = f6;  
-    afn.Lknee = f7;
-    afn.Rhip2 = MirrorSingleCPG(f3);
-    afn.Rankle1 = MirrorSingleCPG(f33);
-    afn.Rknee = MirrorSingleCPG(f4);
+    % sine generation structures for 'antiphase'
+    afn.Lankle2 = f2; % mirror of right frontal ankle in 'phase'
+    afn.Lhip1 = f2; % mirror of right frontal hip in 'phase'
+    afn.Rankle2 = MirrorSingleCPG(f1); % mirror of left frontal ankle in 'phase'
+    afn.Rhip1 = MirrorSingleCPG(f1); % mirror of left frontal hip in 'phase'
+    afn.Lhip2 = f5; % mirror of right sagittal hip in 'phase'
+    afn.Lankle1 = f6; % mirror of right sagittal ankle in 'phase'
+    afn.Lknee = f7; % mirror of right knee in 'phase'
+    afn.Rhip2 = MirrorSingleCPG(f3); % mirror of left sagittal hip in 'phase'
+    afn.Rankle1 = MirrorSingleCPG(f33); % mirror of left sagittal hip in 'phase'
+    afn.Rknee = MirrorSingleCPG(f4); % mirror of left knee in 'phase'
 
-    pfn.Lhip3 = f8;
-    afn.Lhip3 = MirrorSingleCPG(f8);
-    pfn.Rhip3 = f8;
-    afn.Rhip3 = MirrorSingleCPG(f8);
+    pfn.Lhip3 = f8; % left transverse hip (phase)
+    afn.Lhip3 = MirrorSingleCPG(f8); % left transverse hip (antiphase)
+    pfn.Rhip3 = f8; % right transverse hip (phase)
+    afn.Rhip3 = MirrorSingleCPG(f8); % right transverse hip (antiphase)
 end
 
 function waveparams = SingleCPG(offset,scale,in_offset,in_scale)
-% this function creates a structure containing parameters for a sine wave
-% to use for CPGs. 
+% this subfunction creates a structure containing parameters
+% sine function-based CPG.
+% sine function: theta = a + b*sin(c + d*x)
+% where a = offset, b = scale, c = in_offset, d = in_scale
 
+    % set 'offset' parameter to zero, if no offset is given
     if ~isempty(offset)
         waveparams.offset = offset;
     else
         waveparams.offset = 0;
     end
 
+    % set 'scale' parameter to one, if no scale is given
     if ~isempty(scale)
         waveparams.scale = scale;
     else
         waveparams.scale = 1;
     end
 
+    % set 'in_offset' parameter to zero, if no in_offset is given
     if ~isempty(in_offset)
         waveparams.in_offset = in_offset;
     else
         waveparams.in_offset = 0;
     end
 
+    % set 'in_scale' parameter to one, if no in_scale is given
     if ~isempty(in_scale)
         waveparams.in_scale = in_scale;
     else
@@ -107,9 +119,10 @@ function waveparams = SingleCPG(offset,scale,in_offset,in_scale)
 end
 
 function waveparams = MirrorSingleCPG(f1)
-% this function creates a structure of a mirror CPG of the
+% this subfunction creates a structure of a mirror CPG of the
 % CPG represented by the inputted parameters
 
+    % mirroring is implemented by multiplying 'offset' and 'scale' by -1
     waveparams.offset = -f1.offset;
     waveparams.scale = -f1.scale;
     waveparams.in_offset = f1.in_offset;
